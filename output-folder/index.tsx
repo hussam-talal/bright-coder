0e3b1af714004c3208ae957490f751e272b16f82
husam-talal <hhhusam777056229@gmail.com>
2024-09-16 00:23:51 +0300

    bright-coder-ios

--- output-folder/index.tsx
+++
@@ -1,8 +0,0 @@
-// import { AppRegistry } from "react-native";
-// import App from "./App";
- //import expo { name as appName } from "./app.json";
-// import { register } from "@videosdk.live/react-native-sdk";
-
-// register();
-
-// AppRegistry.registerComponent(appName, () => App);
